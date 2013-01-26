33cf666488f8c77620d5f0a8866e9bfcb35160ab
Student <Student@N24570.home>
2013-01-26 17:28:16 +0000

    thats it for now

--- assignment3/ScratchPad.java
+++ assignment3/ScratchPad.java
@@ -2,22 +2,41 @@ package assignment3;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ScratchPad {
+	private Set<Contact> people;
 
-
-	public void launch(){
+	public void dateFinder(){
 		Calendar date = new GregorianCalendar(2013, 0, 21);
 		System.out.println("Today's date is " +  date.get(Calendar.DAY_OF_MONTH) + "/" + 
 		date.get(Calendar.MONTH) + "1/" +  date.get(Calendar.YEAR));
 		
 	}
+	
+	public int varargs(int... numbers){
+		int result = 0;
+		for(int number : numbers){
+			result = result + number;
+		}
+		return result;
+	}
+	
+
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		ScratchPad run = new ScratchPad();
-		run.launch();
+		run.dateFinder();
+		System.out.println(run.varargs(43,6,5,43));
+		
+		Contact bob = new ContactImpl("bob", 1);
+		Contact sally = new ContactImpl("sally", 2);
+		
 		
 		
 	}
